d5e0d256dd29beaf2cbbff991bac8162e2f66ec5
Jesse Giammattolo <giammattolo@hotmail.com>
2020-01-31 08:41:48 -0500

    added JS functionality and more styling. No LS

---
+++ app.js
@@ -0,0 +1,58 @@
+const cost = document.querySelector('#costInput');
+const days = document.querySelector('#daysInput');
+const numOfCigs = document.querySelector('#numOfCigs')
+const resultsBTN = document.querySelector('#btn');
+const clearBTN = document.querySelector('#clearBTN')
+const howLong = document.querySelector('.howLongP');
+const moneySaved = document.querySelector('.moneySaved')
+const numberCigs = document.querySelector('.numberCigs')
+
+const calculate = () => {
+  document.querySelector('.results').style.display = 'flex';
+  const userCost = cost.value;
+  let calendar = days.value;
+  const cigAmount = numOfCigs.value;
+// Convert dates to milliseconds then get days
+  const getDate = new Date();
+  const month = getDate.getUTCMonth() + 1; 
+  const day = getDate.getUTCDate();
+  const year = getDate.getUTCFullYear();
+  newdate = year + "-" + month + "-" + day;
+// calculates total days
+  const calendarMilliSeconds = new Date(calendar)
+  const newdateMilliseconds = new Date(newdate)
+  const totalDaysQuit = (newdateMilliseconds - calendarMilliSeconds) /  86400000
+  howLong.innerHTML = `<p>You've quit smoking for <strong>${totalDaysQuit.toFixed(0)}</strong> days!</p>`
+
+  // calculate total money saved since quit day
+  const eachCig = userCost / 25;
+  const pricePerDay = eachCig * cigAmount
+  const totalMoneySaved = pricePerDay * totalDaysQuit
+  moneySaved.innerHTML =`<p>You've saved <strong>$${totalMoneySaved.toFixed(2)}</strong></p>`
+
+// calculate amount of cigarettes not smoked since quit day
+  const cigarettesNotSmoked = cigAmount * totalDaysQuit
+  numberCigs.innerHTML = `<p><strong>${cigarettesNotSmoked.toFixed(0)}</strong> Cigarettes not smoked!</p>`
+  
+  cost.value = '';
+  days.value = '';
+  numOfCigs.value = '1';
+  resultsBTN.disabled = true;
+}
+
+const clearValues = () => {
+  totalDaysQuit = 0;
+  totalMoneySaved = 0;
+  cigarettesNotSmoked = 0;
+  howLong.innerHTML = '';
+  moneySaved.innerHTML = ''
+  numberCigs.innerHTML = '';
+  cost.value = '';
+  days.value = '';
+  numOfCigs.value = '1';
+  resultsBTN.disabled = false;
+}
+
+resultsBTN.addEventListener('click', calculate)
+clearBTN.addEventListener('click', clearValues)
+
